32281859c7b024e6ee44c23b541e4ee6b0dbbd61
felix <fperez2020@outlook.com>
2024-02-28 16:29:08 -0500

    Daily activites

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,19 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+let promises = [];
+
+array2D.forEach((row) => {
+    const negPromise = new Promise((resolve, reject) => {
+        if (row.filter((num) => num < 0).length > 0) {
+            resolve(row)
+        }
+    });
+    promises.push(negPromise);
+});
+
+Promise.any(promises)
+.then((response) => {
+    console.log(`Negative Row: ${response}`);
+});
--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -3,3 +3,22 @@ const array2D = [
     [4, 5, 6],
     [7, 8, 9]
 ];
+
+let promises = [];
+
+array2D.forEach((row) => {
+    const sumPromise = new Promise((resolve, reject) => {
+        sum = 0;
+        row.forEach((num) => sum += num);
+        resolve(sum);
+    });
+    promises.push(sumPromise);
+});
+
+Promise.all(promises)
+.then((responses) => {
+    sum = 0;
+    responses.forEach((num) => sum += num);
+    console.log(`2D Array Sum: ${sum}`);
+})
+.catch((error) => console.log(error));
